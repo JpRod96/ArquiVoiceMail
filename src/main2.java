b84a23b9a9368434c2ccba56f61aa829f2ed5703
abel555 <taqui555@gmail.com>
2018-05-28 00:51:23 -0400

    delete useless method

--- src/main2.java
+++ src/main2.java
@@ -31,12 +31,6 @@ public class main2 {
         });
 
     }
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+  
 }
 
